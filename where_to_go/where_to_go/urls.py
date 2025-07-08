fe139331e403db5cd8f5b7416ffeab0d86aa5611
1ns0mn1a7 <tyrischev.d.v@gmail.com>
2025-07-08 03:06:31 +0300

    Подключён редактор TinyMCE для удобного редактирования полного описания локаций

--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include
 from .views import index, place_details
 
 
@@ -25,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('places/<int:place_id>/', place_details, name='place_details'),
+    path('tinymce/', include('tinymce.urls')),
 ]
 
 if settings.DEBUG:
